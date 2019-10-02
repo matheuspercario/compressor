cd898401eab9b0cbded3ba9512928e4b12e8e5fe
Matheus Bruder <matheuspercario@gmail.com>
2019-10-02 12:19:32 -0300

    Criada a estrutura do programa, com base em argumentos da linha de comando

--- compressor.c
+++ compressor.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 
 /*
  *		Disciplina ST562 - Estrutura de Arquivos
- *		
+ *		Professor: Celmar Guimaraes da Silva
+ *
+ *
  * 		Projeto - Compressor
  *
  *		Desenvolvedores:
@@ -13,8 +16,61 @@
  *
  */
 
+//-----------------------------------------------------------------------------
+//	Prototipo das Funcoes
+//-----------------------------------------------------------------------------
+	void compressao(char * argumento);
+	void descompressao(char * argumento);
 
-int main (int argc, char* argv[])
+	
+//-----------------------------------------------------------------------------
+//	Funcao Principal - MAIN
+//-----------------------------------------------------------------------------
+int main (int argc, char * argv[])
+{	
+	// Variaveis de controle
+	char * comprimir = "-c";
+	char * descomprimir = "-d";
+	
+
+	if(argc != 3)
+	{
+		// Nao possui numero minimo de argumentos
+		printf("Faltando argumentos\n");
+	}
+	else if(strcmp(argv[1], comprimir) == 0)
+	{
+		// Chamada função de compressao
+		printf("Funcao Compressao\n");
+		compressao(argv[2]);
+	} 
+	else if(strcmp(argv[1], descomprimir) == 0)
+	{
+		// Chamada função de descompressao
+		printf("Funcao Descompressao\n");
+		descompressao(argv[2]);
+	} else
+		{
+			//Parametro invalido
+			printf("Argumento invalido!\n");
+		}
+	
+
+	return 0;	
+}
+
+
+//-----------------------------------------------------------------------------
+//	Funcoes - Compressao e Descompressao
+//-----------------------------------------------------------------------------	
+void compressao(char * argumento)
 {
-	printf("Hello World!\n");
+	// Teste
+	printf("%s\n", argumento);
+}
+
+void descompressao(char * argumento)
+{
+	// Teste
+	printf("%s\n", argumento);
 }

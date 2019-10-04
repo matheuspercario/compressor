5e48016a00d4b0c3d7371b7efc14eec5a0026cf3
Matheus Bruder <matheuspercario@gmail.com>
2019-10-03 21:34:00 -0300

    Nada

--- compressor.c
+++ compressor.c
@@ -13,8 +13,10 @@
  *			Kevin Barrios
  *			Matheus Bruder
  *
- *
  */
+
+	FILE * arq_entrada, arq_saida;
+	char palavra[4096];
 
 //-----------------------------------------------------------------------------
 //	Prototipo das Funcoes
@@ -26,36 +28,34 @@
 //-----------------------------------------------------------------------------
 //	Funcao Principal - MAIN
 //-----------------------------------------------------------------------------
-int main (int argc, char * argv[])
-{	
+int main (int argc, char * argv[]) {	
 	// Variaveis de controle
 	char * comprimir = "-c";
 	char * descomprimir = "-d";
 	
 
-	if(argc != 3)
-	{
+	if(argc != 3) {
 		// Nao possui numero minimo de argumentos
 		printf("Faltando argumentos\n");
 	}
-	else if(strcmp(argv[1], comprimir) == 0)
-	{
-		// Chamada função de compressao
-		printf("Funcao Compressao\n");
-		compressao(argv[2]);
+	else if(strcmp(argv[1], comprimir) == 0) {
+		// Chamada funcao de compressao
+		abertura(argv[1], ".cmp");
+		while(!feof(arq_entrada)) {
+			ler_arq();
+			compressao(argv[2]);
+			gravar_arq();
+		}
 	} 
-	else if(strcmp(argv[1], descomprimir) == 0)
-	{
-		// Chamada função de descompressao
-		printf("Funcao Descompressao\n");
+	else if(strcmp(argv[1], descomprimir) == 0) {
+		// Chamada funcao de descompressao
+		abertura(argv[1], "");
 		descompressao(argv[2]);
-	} else
-		{
+	} else {
 			//Parametro invalido
 			printf("Argumento invalido!\n");
 		}
 	
-
 	return 0;	
 }
 
@@ -63,14 +63,39 @@ int main (int argc, char * argv[])
 //-----------------------------------------------------------------------------
 //	Funcoes - Compressao e Descompressao
 //-----------------------------------------------------------------------------	
-void compressao(char * argumento)
-{
-	// Teste
-	printf("%s\n", argumento);
+void abertura(char * argumento, char * action) {
+	arq_entrada = fopen(argumento, "r");
+	if (arq_entrada == NULL)
+    {
+        printf("Cannot open file!\n");
+        exit(0);
+    }
+	strcat(argumento, action);
+	arq_saida = fopen(argumento, "w");
 }
 
-void descompressao(char * argumento)
-{
+void ler_arq() {
+	fread(&palavra, sizeof(palavra), 1, arq_entrada);
+}
+
+void grava_arq() {
+	fwrite(&palavra, sizeof(palavra), 1, arq_saida);
+}
+
+
+
+
+void compressao(char * argumento) {
 	// Teste
 	printf("%s\n", argumento);
+	printf("Funcao Compressao\n");
 }
+
+void descompressao(char * argumento) {
+	// Teste
+	printf("%s\n", argumento);
+	printf("Funcao Descompressao\n");
+}
+
+
+
